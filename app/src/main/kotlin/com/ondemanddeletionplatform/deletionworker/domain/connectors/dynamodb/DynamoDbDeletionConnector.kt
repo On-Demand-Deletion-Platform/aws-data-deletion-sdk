5a6466dcb3253701012a6a1331e9a862a71fdc74
Mark Sayson <masayson@gmail.com>
2026-01-08 16:19:04 -0800

    Extracted skeleton DynamoDbScanDeletionStrategy

--- app/src/main/kotlin/com/ondemanddeletionplatform/deletionworker/domain/connectors/dynamodb/DynamoDbDeletionConnector.kt
+++ app/src/main/kotlin/com/ondemanddeletionplatform/deletionworker/domain/connectors/dynamodb/DynamoDbDeletionConnector.kt
@@ -4,7 +4,6 @@ import aws.sdk.kotlin.services.dynamodb.DynamoDbClient
 import com.ondemanddeletionplatform.deletionworker.domain.models.dynamodb.DynamoDbDeletionKeyValue
 import com.ondemanddeletionplatform.deletionworker.domain.models.dynamodb.DynamoDbDeletionStrategyType
 import com.ondemanddeletionplatform.deletionworker.domain.models.dynamodb.DynamoDbDeletionTarget
-import com.ondemanddeletionplatform.deletionworker.domain.models.dynamodb.ValidatedDynamoDbScanDeletionTarget
 
 /**
  * Used to execute on-demand data deletion requests
@@ -15,13 +14,7 @@ class DynamoDbDeletionConnector(val ddb: DynamoDbClient) {
     when (deletionTarget.strategy) {
       DynamoDbDeletionStrategyType.TABLE_KEY -> DynamoDbTableKeyDeletionStrategy().deleteData(ddb, deletionTarget, deletionKey)
       DynamoDbDeletionStrategyType.GSI_QUERY -> DynamoDbGsiKeyDeletionStrategy().deleteData(ddb, deletionTarget, deletionKey)
-      DynamoDbDeletionStrategyType.SCAN -> deleteCustomerByScan(deletionTarget, deletionKey)
+      DynamoDbDeletionStrategyType.SCAN -> DynamoDbScanDeletionStrategy().deleteData(ddb, deletionTarget, deletionKey)
     }
-  }
-
-  private suspend fun deleteCustomerByScan(deletionTarget: DynamoDbDeletionTarget, deletionKey: DynamoDbDeletionKeyValue) {
-    println("Called scan deletion for deletionKey: $deletionKey")
-    ValidatedDynamoDbScanDeletionTarget.fromDeletionTarget(deletionTarget)
-    throw NotImplementedError("Scan deletion strategy not yet implemented")
   }
 }
